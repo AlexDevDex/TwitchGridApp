5f1825206df8fbf7d71468fadc1afa9e51d5917f
Alex <alexander.f.walter@gmail.com>
2023-11-15 04:47:50 +0100

    fix

--- gridApp_discord_bot.py
+++ gridApp_discord_bot.py
@@ -1,9 +1,8 @@
 import os
 import discord
 from discord.ext import commands
-from config import DISCORD_TOKEN
 
 class DiscordBot(commands.Bot):
     def __init__(self, prefix):
         super().__init__(command_prefix=prefix)
-        self.token = DISCORD_TOKEN
+        #self.token = DISCORD_TOKEN
